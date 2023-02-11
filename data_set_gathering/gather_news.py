e8ad038a1f97ed9299295a5848a46ae1ae3f0623
kavchan <46276998+kavchan@users.noreply.github.com>
2023-02-11 07:59:06 -0600

    Added info column
    Added info column for manual scraping if prompt content isn't correct

--- data_set_gathering/gather_news.py
+++ data_set_gathering/gather_news.py
@@ -24,19 +24,24 @@ sheet = workbook.active
 news_articles = api.get_everything(q='Nvidia', language='en') # News Article is a nested dictionary
 
 # These column titles are necessary for putting the xlsx in a format recognizable by the OpenAI CLI data preparation tool
-sheet["A1"] = "prompt"
-sheet["B1"] = "completion"
+sheet["A1"] = "Article Info"
+sheet["B1"] = "prompt"
+sheet["C1"] = "completion"
 
 row = 2 # began at the next row
+
+# extract the article text from each news article, if URL request timesout write information to xlsx
 for news in news_articles.get('articles'):
     try :
         url = news.get('url')
         article = g.extract(url=url)
         text = article.cleaned_text
-        sheet.cell(row=row, column=1, value= 'Published: ' + news.get('publishedAt') + '\n Title: ' + news.get('title') + '\n URL: ' + news.get('url') + '\n Content: \n\n' + text)
+        sheet.cell(row=row, column=1, value = 'Date Published: ' + news.get('publishedAt') + '\nTitle: ' + news.get('title') + '\nURL: ' + news.get('url'))
+        sheet.cell(row=row, column=2, value = 'Date Published: ' + news.get('publishedAt') + '\nContent:\n\n' + text)
         row += 1
     except requests.exceptions.ReadTimeout:
-        sheet.cell(row=row, column=1, value= '**URL TIMEOUT**, \n\n Published: ' + news.get('publishedAt') + '\n Title: ' + news.get('title') + '\n URL: ' + news.get('url') + '\n Truncated Content: ' + news.get('content'))
+        sheet.cell(row=row, column=1, value= '**URL TIMEOUT**, \n\n Date Published: ' + news.get('publishedAt') + '\nTitle: ' + news.get('title') + '\nURL: ' + news.get('url'))
+        sheet.cell(row=row, column=2, value = 'Date Published: ' + news.get('publishedAt') + '\nTruncated Content: ' + news.get('content'))
         row += 1
 
 workbook.save("sample_fine_tune.xlsx")
