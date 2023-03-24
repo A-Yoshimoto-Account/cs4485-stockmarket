f4c87a953dff75eaba48770ccb805e0880153934
ayoshi007 <aki.f.yoshimoto@gmail.com>
2023-03-23 20:50:11 -0500

    Added functionality to upload properly formatted CSVs

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import brains.openai_api.openai_controller as openai_api
 
-DISABLE_EMBEDDIGNS = True
+DISABLE_EMBEDDINGS = True
 DISABLE_COMPLETIONS = True
 GPT_MODEL = 'gpt-3.5-turbo'
 ENDPOINT = openai_api.CHATCOMPLETION
--- static/app.js
+++ static/app.js
@@ -13,6 +13,7 @@ function updateTextArea() {
 		'ksim': document.getElementById('kSimRange').value,
 		'memory': document.getElementById('memoryRange').value,
 	};
+	document.querySelector("input[name='question']").value = '';
 	console.log(data);
 	const url = '/process_question';
 	fetch(url, {
@@ -42,7 +43,7 @@ function downloadConvo() {
 	let data = getConversation();
 	let file = new Blob([data], type=type);
 	let modelName = document.getElementById('modelName').innerHTML;
-	let now = getDateTimeNow();	
+	let now = getDateTimeNow();
 	let filename = modelName + '_' + now + '.csv'
 	let url = URL.createObjectURL(file);
 
@@ -57,14 +58,17 @@ function downloadConvo() {
 
 }
 function getConversation() {
-	let rows = 'query,response\n'
 	queryDivs = document.getElementsByClassName("user-queries")
 	responseDivs = document.getElementsByClassName("model-responses");
+	rows = '';
 	Array.from(queryDivs).forEach((queryDiv, index) => {
 		responseDiv = responseDivs[index];
 		query = queryDiv.getElementsByTagName('p')[0].innerHTML;
 		response = responseDiv.getElementsByTagName('p')[0].innerHTML;
-		rows += query + ',' + response + '\n';
+		rows += query + ',' + response;
+		if (index < queryDivs.length - 1) {
+			rows += '\n';
+		}
 	});
 
 	return rows;
@@ -82,8 +86,39 @@ function getDateTimeNow() {
 }
 
 function uploadSavedConvo() {
-    let convoFile = document.getElementById("savedConvoFile").files[0];
-    console.log("Uploaded " + convoFile.name + " as convo memory");
+    const convoFile = document.getElementById("savedConvoFile").files[0];
+	if (convoFile == null) {
+		return;
+	}
+	document.getElementById("savedConvoFile").value = '';
+
+	let csvReader = new FileReader();
+	csvReader.onload = function() {
+		const fileText = csvReader.result
+		const url = '/upload_convo';
+		fetch(
+			url, 
+			{
+				'method': 'POST',
+				'headers': {'Content-Type': 'application/json'},
+				'body': JSON.stringify(fileText)
+			}
+		).then(response => response.json())
+		.then(resp => {
+			let chatArea = document.getElementById('chat-area');
+			if (resp['ok']) {
+				rows = fileText.split('\n');
+				rows.forEach((row) => {
+					content = row.split(',');
+					chatArea.innerHTML += createConvoElement(content[0], content[1]);	
+				})
+			}
+			chatArea.innerHTML += createSystemMessage(resp['type'], resp['message']);
+			chatArea.scrollTop = chatArea.scrollHeight;
+		})
+	}
+	csvReader.readAsText(convoFile);
+    console.log("Sent " + convoFile.name + " as convo memory");
 }
 
 function clearMemory() {
@@ -92,4 +127,22 @@ function clearMemory() {
 
 function clearAll() {
     console.log("Clears the screen and the internal convo memory");
+}
+
+function createSystemMessage(type, message) {
+	return 	'<div class="system-message">' + 
+			'	<div class="system-messages ' + type + ' text-block">' + 
+			'		<b>System:&nbsp</b><p>' + message + '</p>' +
+			'	</div>' + 
+			'</div>';
+}
+function createConvoElement(question, answer) {
+	return 	'<div class="query-response">' + 
+			'	<div class="user-queries text-block">' + 
+			'		<b>You:&nbsp</b><p>' + question + '</p>' +
+			'	</div>' + 
+			'	<div class="model-responses text-block">' + 
+			'		<b>Response:&nbsp</b><p>' + answer + '</p>' + 
+			'	</div>' + 
+			'</div>';
 }
