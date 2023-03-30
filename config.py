e7987dc4fd88367fe67b29a91d58e495962fc30d
bagelzcode <garyn@varela.net>
2023-03-30 14:43:09 -0500

    committing for commiting sake test

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import brains.openai_api.openai_controller as openai_api
 
 DISABLE_EMBEDDINGS = True
-DISABLE_COMPLETIONS = False
+DISABLE_COMPLETIONS = True
 GPT_MODEL = 'gpt-3.5-turbo'
 ENDPOINT = openai_api.CHATCOMPLETION
 REFINE = 'list'
