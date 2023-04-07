6a304e706cfea8def4a00b602cfa78668c325c16
bagelzcode <garyn@varela.net>
2023-04-07 13:18:49 -0500

    Added wait and loadenv for startup startup.py now has loadenv and wait

--- startup.py
+++ startup.py
@@ -1,13 +1,17 @@
 import os
 import subprocess
+import time
+import dotenv
 
 def main():
+    dotenv.load_dotenv()
     cur = os.getcwd()
     path = os.path.join(cur,'milvus_db')
+    path_two = os.path.join(cur,'brains\data_set_gathering')
     subprocess.check_call('python -m pip install -r requirements.txt"')
     subprocess.check_call('python -m pip install .')
     subprocess.check_call("docker compose up -d", cwd = path)
-    path_two = os.path.join(cur,'brains\data_set_gathering')
+    time.sleep(60)
     subprocess.check_call(f"python {path_two}\gather_news.py")
     subprocess.check_call("python config_db.py", cwd = path)
     subprocess.Popen("flask run")
