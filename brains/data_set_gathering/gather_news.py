458eadcdb38f26486c802b1a21b71c81cf5e44bf
kavchan <46276998+kavchan@users.noreply.github.com>
2023-04-14 10:59:20 -0500

    Refactored Code
    Reduced method sizes.
    Separated file path creation for embeds & file to method. Separated created row data to separate method.

--- brains/data_set_gathering/gather_news.py
+++ brains/data_set_gathering/gather_news.py
@@ -18,12 +18,9 @@ NEWS_API_QUERY = ['Nvidia market', 'AMD market', 'Intel Market', 'ARM Holdings',
 api = NewsApiClient(api_key=os.getenv("NEWS_API_KEY"))
     
 def create_context_csv():
-    today = datetime.today().strftime('%m-%d-%Y')
-    directory = 'milvus_db\initial_data'
-    file_name = f'context_{today}.csv'
-    embed_file_name = f'embeds_{today}.csv'
-    file_path = os.path.join(directory, file_name)
-    embed_file_path = os.path.join(directory, embed_file_name)
+
+    file_path = create_file_path('context')
+    embed_file_path = create_file_path('embeds')
     
     if not os.path.exists(file_path):
         col_headers = ['URL','Title','Date','Content']
@@ -38,18 +35,25 @@ def create_context_csv():
         clean_csv(file_path, embed_file_path)
     else:
         print("This file already exists.")
+        #after file is created, it is cleaned
+        clean_csv(file_path, embed_file_path)
 
+def create_file_path(file_type: str):
+        today = datetime.today().strftime('%m-%d-%Y')
+        directory = 'milvus_db\initial_data'
+        file_name = f'{file_type}_{today}.csv'
+        return os.path.join(directory, file_name)
+    
 def create_content_list(queries=NEWS_API_QUERY, domains=WHITELIST, excludeDomains=BLACKLIST) :
     content_list = []
     news_articles_list = []
     seen_urls = set() # To prevent duplicate articles
     for news_query in queries:
         news_articles_list.append(api.get_everything(q=news_query, language='en', domains=domains, exclude_domains=excludeDomains, sort_by='relevancy')) # News Article is a nested dictionary
-    g = Goose()
-    count = 0
+    index = 0
     for news_articles in news_articles_list:
-        print(f"Getting article contents for '{queries[count]}'...")
-        count += 1
+        print(f"Getting article contents for '{queries[index]}'...")
+        index += 1
         progress = 1
         for news in news_articles.get('articles'):
             try :
@@ -57,17 +61,7 @@ def create_content_list(queries=NEWS_API_QUERY, domains=WHITELIST, excludeDomain
                 if url in seen_urls:
                     continue
                 else:
-                    title = news.get('title')
-                    date = news.get('publishedAt')
-                    splitdate = date.split('T')
-                    ymd = splitdate[0] # ymd = year month day
-                    content = g.extract(url).cleaned_text
-                    row_data = []
-                    row_data.append(url)
-                    row_data.append(title)
-                    row_data.append(ymd)
-                    row_data.append(content)
-                    content_list.append(row_data)
+                    content_list.append(create_row_data(news))
                     seen_urls.add(url)
             except requests.exceptions.ReadTimeout:
                 print('News API Read Timeout')
@@ -75,6 +69,21 @@ def create_content_list(queries=NEWS_API_QUERY, domains=WHITELIST, excludeDomain
                 print(f"Progress: [{progress}/{len(news_articles.get('articles'))}]", end="\r")
                 progress += 1         
     return content_list
+
+def create_row_data(news_article) :
+    g = Goose()
+    title = news_article.get('title')
+    date = news_article.get('publishedAt')
+    splitdate = date.split('T')
+    ymd = splitdate[0] # ymd = year month day
+    url = news_article.get('url')
+    content = g.extract(url).cleaned_text
+    row_data = []
+    row_data.append(url)
+    row_data.append(title)
+    row_data.append(ymd)
+    row_data.append(content)
+    return row_data
 
 def main() :
     create_context_csv()
