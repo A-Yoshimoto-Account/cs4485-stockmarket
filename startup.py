a2baf9d1cab69f7bb0bb4272aada541e0d1d3dbd
ayoshi007 <89048791+ayoshi007@users.noreply.github.com>
2023-04-20 20:54:42 -0500

    Changed timing of dotenv import, changed check_calls to use lists for Linux

--- startup.py
+++ startup.py
@@ -1,18 +1,20 @@
 import os
 import subprocess
 import time
-import dotenv
+
+SLEEP = 15
 
 def main():
-    dotenv.load_dotenv()
     cur = os.getcwd()
     path = os.path.join(cur,'milvus_db')
     path_two = os.path.join(cur,'brains/data_set_gathering')
     subprocess.check_call(['python', '-m', 'pip', 'install', '-r', 'requirements.txt'])
+    import dotenv
+    dotenv.load_dotenv()
     subprocess.check_call(['python', '-m', 'pip', 'install', '.'])
     subprocess.check_call(['docker', 'compose', 'up', '-d'], cwd = path)
-    print('Waiting 60 secs for Docker to warm up')
-    time.sleep(60)
+    print(f'Waiting {SLEEP} secs for Docker to warm up')
+    time.sleep(SLEEP)
     subprocess.check_call(['python', f'{path_two}/gather_news.py'])
     subprocess.check_call(['python', 'config_db.py'], cwd = path)
     subprocess.run(['flask', 'run'])
