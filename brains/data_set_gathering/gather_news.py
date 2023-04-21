582436c4361bae12cb2b2e337066da680cc421bf
kavchan <46276998+kavchan@users.noreply.github.com>
2023-04-21 14:42:11 -0500

    Commented gather_news.py

--- brains/data_set_gathering/gather_news.py
+++ brains/data_set_gathering/gather_news.py
@@ -16,7 +16,14 @@ BLACKLIST = 'kotaku.com, gizmodo.com, digitaltrends.com, stocknews.com, barrons.
 NEWS_API_QUERY = ['Nvidia market', 'AMD market', 'Intel Market', 'ARM Holdings', 'TSMC Market', 'Semiconductor Market', 'Qualcomm', 'Micron Technologies'] 
 
 api = NewsApiClient(api_key=os.getenv("NEWS_API_KEY"))
-    
+
+"""
+Creates context CSV for loading into Milvus DB.
+Args:
+    None
+Returns:
+    None
+"""   
 def create_context_csv():
 
     file_path = create_file_path('context')
@@ -29,12 +36,26 @@ def create_context_csv():
     else:
         print(f"The file {file_path} already exists. Skipping gathering articles.")
 
+"""
+Returns a file path for the given file type, including the current date.
+Args:
+    file_type (str): The type of file to create a path for.
+Returns:
+    str: The file path as a string.
+"""
 def create_file_path(file_type: str):
         today = datetime.today().strftime('%m-%d-%Y')
         directory = 'milvus_db/initial_data'
         file_name = f'{file_type}_{today}.csv'
         return os.path.join(directory, file_name)
-    
+
+"""
+Returns a file path for the given file type, including the current date.
+Args:
+    file_type (str): The type of file to create a path for.
+Returns:
+    str: The file path as a string.
+"""
 def write_file(path, contents: list) :
     col_headers = ['URL','Title','Date','Content']
     print('Writing to file..')
@@ -43,6 +64,15 @@ def write_file(path, contents: list) :
         writer.writerow(col_headers)
         writer.writerows(contents)
 
+"""
+Returns a content list from NewsAPI with the given news query list.
+Args:
+    queries (list): A list of queries to search for.
+    domains (list) : A list of domains to search for.
+    excludeDomains (list) : A list of domains to exclude from the search.
+Returns:
+    content (list): A list of lists containing the URL, title, date, and content of each article.
+"""
 def create_content_list(queries=NEWS_API_QUERY, domains=WHITELIST, excludeDomains=BLACKLIST) :
     content_list = []
     news_articles_list = []
@@ -66,7 +96,14 @@ def create_content_list(queries=NEWS_API_QUERY, domains=WHITELIST, excludeDomain
             progress += 1         
     return content_list
 
-
+"""
+Constructs row data from the given news object.
+Args:
+    n (dict): News object from NewsAPI.
+    g (Goose): Goose object for extracting article content.
+Returns:
+    row_data (list): A list containing the URL, title, date, and content of the article.
+"""
 def row_data(n, g) :
     title = n.get('title')
     date = n.get('publishedAt')
